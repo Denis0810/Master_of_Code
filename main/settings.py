6b8f08ee703513819d1e6b0b2766094405f3c1a2
Denys Stupak <stupak.denys@i.ua>
2018-01-24 02:32:56 +0200

    Implement ZIPCode to coords

--- main/settings.py
+++ main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['testwork/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- testwork/views.py
+++ testwork/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+import requests
+from django.views.generic import FormView, TemplateView
+from django.http import JsonResponse
+from .forms import ZIPForms
 
-# Create your views here.
+
+class Main(FormView):
+    template_name = 'testwork/index.html'
+    form_class = ZIPForms
+
+
+def get_coords(request):
+    zip_code = request.GET.get('zip_code', None)
+    text_request = 'https://www.zipcodeapi.com/rest/' + \
+                   '4mMHEGPGWlElFlDAYb1ttdPqAuL8f2Py13VVg7HUXvmPFXVWogwQubYz1D0wYqQn/info.json/' + \
+                   zip_code + '/degrees'
+
+    resp = requests.get(text_request)
+    msg = ''
+    lat = ''
+    lng = ''
+    if resp.status_code != 200:
+        msg = resp.json()['error_msg']
+    else:
+        lat = resp.json()['lat']
+        lng = resp.json()['lng']
+
+    data = {
+        'lat': lat,
+        'lng': lng,
+        'msg': msg
+    }
+    return JsonResponse(data)
